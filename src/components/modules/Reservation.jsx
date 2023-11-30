87fc28bd79266d8aead911bb049c0f39e9bbb0e3
hibamtol1 <hibamtol1@gmail.com>
2023-11-30 23:39:19 +0900

    입력받아야할 항목 정의

--- src/components/modules/Reservation.jsx
+++ src/components/modules/Reservation.jsx
@@ -1,3 +1,25 @@
 export default function Reservation() {
-  return <h1>예약하기</h1>;
+  return (
+    <div>
+      <h1>예약하기</h1>
+      <div>
+        <b>이름</b><input />
+      </div>
+      <div>
+        <b>시작일자</b><input />
+      </div>
+      <div>
+        <b>종료일자</b><input />
+      </div>
+      <div>
+        <b>예약장소</b><input />
+      </div>
+      <div>
+        <b>전화번호</b><input />
+      </div>
+      <div>
+        <button>신청</button>
+      </div>
+    </div>
+  );
 }
