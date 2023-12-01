f0280d8d2864f00bcc2ec1057a53b2c3049c4a14
hibamtol1 <hibamtol1@gmail.com>
2023-12-01 11:57:01 +0900

    예약장소 input을 Select Box로 변경

--- src/components/modules/Reservation.jsx
+++ src/components/modules/Reservation.jsx
@@ -1,4 +1,14 @@
 export default function Reservation() {
+  const SelectBox = () => {
+    return (
+      <select>
+        <option key="" value="">1층 J4</option>
+        <option key="" value="">3층 소회의실</option>
+        <option key="" value="">기업지원허브</option>
+      </select>
+    );
+  };
+
   return (
     <div>
       <h1>예약하기</h1>
@@ -12,7 +22,7 @@ export default function Reservation() {
         <b>종료일자</b><input />
       </div>
       <div>
-        <b>예약장소</b><input />
+        <b>예약장소</b><SelectBox />
       </div>
       <div>
         <b>전화번호</b><input />
