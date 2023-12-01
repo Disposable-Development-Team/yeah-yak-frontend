95edf7f0e938feeb5ac630a51d486b4b8eacd5be
makerseok <maker7788@gmail.com>
2023-12-02 02:55:24 +0900

    외부 데이터 Table로 표시
    기존 정의해둔 Table module을 사용해 api로 받아온 데이터 Table로 표시

--- src/components/templates/Admin/Confirm.jsx
+++ src/components/templates/Admin/Confirm.jsx
@@ -1,3 +1,37 @@
+// TableWithApi.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Table, generateTableHeaders, renderTableData } from '@modules/Table';
+
 export default function Confirm() {
-  return <h1>어드민 예약 확인 창입니다...</h1>;
+  const [reservationData, setReservationData] = useState([]);
+
+  axios
+    .get('http://140.238.0.188:3001/reservations')
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://140.238.0.188:3001/reservations');
+        setReservationData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Table>
+      <thead>{generateTableHeaders(reservationData)}</thead>
+      <tbody>{renderTableData(reservationData)}</tbody>
+    </Table>
+  );
 }
