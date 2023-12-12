6af6b469d81a52fb3d820ed33cf250fcba63c3e2
hibamotl1 <hibamtol1@gmail.com>
2023-12-12 16:39:11 +0900

    Input atom 기능 추가

--- src/components/atoms/Input/index.jsx
+++ src/components/atoms/Input/index.jsx
@@ -1,3 +1,9 @@
-export default function Input() {
-  return;
+export default function Input(props) {
+  const contents = props.contents;
+  const status = props.status;
+  return (
+    <div>
+      <input>{contents}</input>
+    </div>
+  )
 }
