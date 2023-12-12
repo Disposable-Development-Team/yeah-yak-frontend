67dee09bb860f97d72f7d383280fee1b96ced5a5
makerseok <maker7788@gmail.com>
2023-12-13 00:20:32 +0900

    컴포넌트의 레이아웃을 조정할 수 있는 Flex atom 추가

---
+++ src/components/atoms/Flex/index.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: ${props => (props.column ? 'column' : 'row')};
+  justify-content: ${props => props.justifyContent || 'flex-start'};
+  align-items: ${props => props.alignItems || 'stretch'};
+  flex-wrap: ${props => (props.noWrap ? 'nowrap' : 'wrap')};
+  gap: ${props => props.gap || '1rem'};
+`;
+
+export const FlexItem = styled.div`
+  flex: ${props => props.flex || ''};
+  margin: ${props => props.margin || '0'};
+  padding: ${props => props.padding || '0'};
+  order: ${props => props.order || '0'};
+`;
