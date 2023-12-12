d8a93fac0dca70d6a53a178da8f111b913fd1b93
hibamtol1 <hibamtol1@gmail.com>
2023-12-12 22:51:12 +0900

    Header 모듈 추가

--- src/components/modules/Header/index.jsx
+++ src/components/modules/Header/index.jsx
@@ -1,11 +1,16 @@
 import Image from "@atoms/Image";
 import Button from "@atoms/Button";
 
-export default function Alert() {
+export default function Header() {
   return(
     <div>
-      <Image></Image>
-      <Button></Button>
+      <Image
+        width="300px"
+        height="200px"
+        src="https://via.placeholder.com/300x200.png?text=SONGJOOWON"
+        link="https://github.com/Disposable-Development-Team/yeah-yak-frontend"
+      />
+      <Button/>
     </div>
   );
 }
