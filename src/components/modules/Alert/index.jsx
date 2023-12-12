27585e540c30518a68bd50dbcc5397a6943a6eb6
hibamtol1 <hibamtol1@gmail.com>
2023-12-12 22:32:08 +0900

    Alert 모듈 기능 추가

--- src/components/modules/Alert/index.jsx
+++ src/components/modules/Alert/index.jsx
@@ -4,10 +4,18 @@ import Button from "@atoms/Button";
 export default function Alert() {
   return(
     <div>
-      <Input></Input>
-      <Input></Input>
-      <Input></Input>
-      <Button></Button>
+      <div>
+        <Input hasInput={true} isReadOnly={true} value="정보입력" />
+      </div>
+      <div>
+        <Input hasInput={true} isReadOnly={false} placeholder="예약자명" />
+      </div>
+      <div>
+        <Input hasInput={true} isReadOnly={false} placeholder="전화번호" />
+      </div>
+      <div>
+        <Button></Button>
+      </div>
     </div>
   );
 }
