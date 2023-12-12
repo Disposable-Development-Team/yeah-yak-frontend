0d8d3e079c1fb7bbbe96bc0d1b2474642fa7a75f
hibamtol1 <hibamtol1@gmail.com>
2023-12-12 23:11:02 +0900

    Reservation 템플릿 추가

--- src/components/templates/Reservation.jsx
+++ src/components/templates/Reservation.jsx
@@ -1,7 +1,29 @@
+import Button from "@atoms/Button";
+import Input from "@atoms/Input";
+import Paragraph from "@atoms/Paragraph";
+
 export default function Reservation() {
   return (
     <div>
-      <h1>예약 페이지</h1>
+      <div>
+        <Paragraph size="24px" weight="bold">예약하기</Paragraph>
+        <Paragraph size="24px" weight="bold">합주실</Paragraph>
+      </div>
+      <div>
+        <Paragraph size="12px">시작일</Paragraph>
+        <Input hasInput={true} isReadOnly={false} placeholder="YYYY-MM-DD"/>
+        <Paragraph size="12px">종료일</Paragraph>
+        <Input hasInput={true} isReadOnly={false} placeholder="YYYY-MM-DD"/>
+      </div>
+      <div>
+        <Input hasInput={true} isReadOnly={false} placeholder="예약자명" />
+      </div>
+      <div>
+        <Input hasInput={true} isReadOnly={false} placeholder="전화번호" />
+      </div>
+      <div>
+        <Button>신청</Button>
+      </div>
     </div>
   )
   ;
