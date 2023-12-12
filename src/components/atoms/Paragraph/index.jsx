08352e0cc10b7916a5fb40fd3e1cdb20a8e4bfc0
hibamtol1 <hibamtol1@gmail.com>
2023-12-12 22:57:18 +0900

    Paragraph atom 추가

--- src/components/atoms/Paragraph/index.jsx
+++ src/components/atoms/Paragraph/index.jsx
@@ -1,3 +1,16 @@
-export default function Paragraph() {
-  return;
-}
+const Paragraph = (props) => {
+  const { size, weight, children } = props;
+
+  const paragraphStyle = {
+    fontSize: size,
+    fontWeight: weight,
+  };
+
+  return (
+    <div style={paragraphStyle}>
+      {children}
+    </div>
+  );
+};
+
+export default Paragraph;
