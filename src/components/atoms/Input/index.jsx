671073e81aeb90dd27c96a4a5d9b6669532754eb
hibamtol1 <hibamtol1@gmail.com>
2023-12-15 12:24:13 +0900

    Input atom styled-components로 변경

--- src/components/atoms/Input/index.jsx
+++ src/components/atoms/Input/index.jsx
@@ -1,3 +1,9 @@
+import styled from 'styled-components';
+
+const StyledInput = styled.input`
+
+`;
+
 const Input = ({ hasInput, isReadOnly, ...restProps }) => {
   // 만약 hasInput이 false이면 input 요소를 생성하지 않습니다.
   if (!hasInput) {
@@ -5,10 +11,7 @@ const Input = ({ hasInput, isReadOnly, ...restProps }) => {
   }
 
   return (
-    <input
-      readOnly={isReadOnly}
-      {...restProps}
-    />
+    <StyledInput readOnly={isReadOnly} {...restProps} />
   );
 };
 
