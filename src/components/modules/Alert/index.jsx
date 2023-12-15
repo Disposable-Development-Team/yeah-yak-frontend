3e3c202b59656c39f2e33c98246f54b9b1004214
hibamtol1 <hibamtol1@gmail.com>
2023-12-15 12:32:13 +0900

    Alert module styled-components로 변경

--- src/components/modules/Alert/index.jsx
+++ src/components/modules/Alert/index.jsx
@@ -1,9 +1,17 @@
-import Input from "@atoms/Input";
-import Button from "@atoms/Button";
+import React from 'react';
+import styled from 'styled-components';
+import Input from '@atoms/Input';
+import Button from '@atoms/Button';
 
-export default function Alert() {
-  return(
-    <div>
+const StyledAlert = styled.div`
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #ccc;
+`;
+
+const Alert = () => {
+  return (
+    <StyledAlert>
       <div>
         <Input hasInput={true} isReadOnly={true} value="정보입력" />
       </div>
@@ -14,8 +22,10 @@ export default function Alert() {
         <Input hasInput={true} isReadOnly={false} placeholder="전화번호" />
       </div>
       <div>
-        <Button/>
+        <Button />
       </div>
-    </div>
+    </StyledAlert>
   );
-}
+};
+
+export default Alert;
