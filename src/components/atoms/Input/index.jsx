32ac04f41b078ba81ab4c125b840536b20f81654
makerseok <maker7788@gmail.com>
2023-12-16 02:29:55 +0900

    Input atom 개선

--- src/components/atoms/Input/index.jsx
+++ src/components/atoms/Input/index.jsx
@@ -1,18 +1,42 @@
+import React, { useId } from 'react';
 import styled from 'styled-components';
 
-const StyledInput = styled.input`
-
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
 `;
 
-const Input = ({ hasInput, isReadOnly, ...restProps }) => {
-  // 만약 hasInput이 false이면 input 요소를 생성하지 않습니다.
-  if (!hasInput) {
-    return null;
-  }
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 8px;
+`;
 
+const Label = styled.label`
+  margin-right: 8px;
+  min-width: 100px;
+`;
+
+const Input = styled.input`
+  padding: 8px;
+  min-width: 187px;
+  border: ${props => (props.readOnly ? 'none' : '1px solid #ccc;')};
+  border-radius: 4px;
+  background-color: ${props => (props.readOnly ? 'transparent' : '#fff')};
+  cursor: ${props => (props.readOnly ? 'default' : 'auto')};
+`;
+
+const LabeledInput = ({ label, readOnly, type, name, value, onChange }) => {
+  const inputId = useId();
   return (
-    <StyledInput readOnly={isReadOnly} {...restProps} />
+    <Container>
+      <Row>
+        {label && <Label htmlFor={inputId}>{label}</Label>}
+        <Input id={inputId} name={name} readOnly={readOnly} type={type} value={value} onChange={onChange} />
+      </Row>
+    </Container>
   );
 };
 
-export default Input;
+export default LabeledInput;
