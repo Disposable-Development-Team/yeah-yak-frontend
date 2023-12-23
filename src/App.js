ddc23026c5411a038e86c9d925fb5440a772c8a6
makerseok <67940953+makerseok@users.noreply.github.com>
2023-12-23 15:37:05 +0900

    Revert "Feature/create reservation history template"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 // react router
 import { Route, Routes, Link } from 'react-router-dom';
 import Main from '@templates/Main';
-import Admin from '@templates/Admin';
-import '@styles/main.css';
-import ReservationHistory from '@templates/ReservationHistory';
+import Confirm from '@templates/Admin/Confirm';
+import Reservation from '@templates/Reservation';
 
 import theme from '@styles/theme';
 import { ThemeProvider } from 'styled-components';
@@ -18,14 +17,14 @@ function App() {
           <Link to="/">
             <Button $outline>홈</Button>
           </Link>
-          <Link to="/admin">
+          <Link to="/admin/confirm">
             <Button color="red">관리자 화면</Button>
           </Link>
 
           <Routes>
             <Route path="/" element={<Main />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="/reservations/history" element={<ReservationHistory />} />
+            <Route path="/reservation" element={<Reservation />} />
+            <Route path="/admin/confirm" element={<Confirm />} />
           </Routes>
         </ThemeProvider>
       </ModalProvider>
--- src/components/modules/Header/index.jsx
+++ src/components/modules/Header/index.jsx
@@ -1,83 +1,22 @@
 import styled from 'styled-components';
 import Image from '@atoms/Image';
 import Button from '@atoms/Button';
-import { FlexContainer } from '@atoms/Flex';
-import { Link } from 'react-router-dom';
-import Modal from '@modules/Modal';
-import { useModalContext } from '@modules/Modal/ModalContext';
-import Form from '@modules/Form';
-import { useState } from 'react';
-import Input from '@atoms/Input';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+
+const StyledHeader = styled.div`
+  text-align: center;
+`;
 
 const Header = () => {
-  const { modalOpen, openModal, closeModal } = useModalContext();
-  const navigate = useNavigate();
-
-  const [values, setValues] = useState({
-    name: '',
-    phoneNumber: '',
-  });
-
-  const handleChange = e => {
-    setValues({
-      ...values,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-
-    try {
-      const userInfo = {
-        name: values.name,
-        phoneNumber: values.phoneNumber,
-      };
-      // Make an API request using Axios
-      const response = await axios.get(
-        `http://140.238.0.188:8080/reservations?name=${values.name}&phonenumber=${values.phoneNumber}`,
-      );
-      console.log(response);
-      // Check if the response code is 1
-      // if (response.data.code === 1) {
-      if (true) {
-        // If code is 1, route to /reservations/history with the reservation data
-        const reservationData = response.data; // Adjust the property based on your API response structure
-        // Use the react-router-dom history object to navigate to the new route
-        closeModal('checkUser');
-        navigate('/reservations/history', { state: { data: reservationData, ...userInfo } });
-      } else {
-        // Handle other cases or show an error message
-        alert('Reservation failed. Please try again.');
-      }
-    } catch (error) {
-      // Handle API request error
-      console.error('Error making API request:', error);
-      alert('An error occurred. Please try again.');
-    }
-  };
-
   return (
-    <FlexContainer $justifyContent="space-between" $alignItems="center">
-      <Image width="150px" height="100px" src="https://via.placeholder.com/300x200.png?text=SONGJOOWON" link="/" />
-
-      <Button $outline onClick={() => openModal('checkUser')}>
-        예약현황
-      </Button>
-      <Modal title="정보입력" modalId="checkUser">
-        <Form submitHandler={handleSubmit}>
-          <FlexContainer $noWrap $gap="0" $column $alignItems="center">
-            <Input label="예약자명" name="name" value={values.name} onChange={handleChange} />
-            <Input label="전화번호" name="phoneNumber" value={values.phoneNumber} onChange={handleChange} />
-          </FlexContainer>
-          <FlexContainer $justifyContent="flex-end">
-            <Button type="submit">확인</Button>
-          </FlexContainer>
-        </Form>
-      </Modal>
-    </FlexContainer>
+    <StyledHeader>
+      <Image
+        width="300px"
+        height="200px"
+        src="https://via.placeholder.com/300x200.png?text=SONGJOOWON"
+        link="https://github.com/Disposable-Development-Team/yeah-yak-frontend"
+      />
+      <Button>예약현황</Button>
+    </StyledHeader>
   );
 };
 
