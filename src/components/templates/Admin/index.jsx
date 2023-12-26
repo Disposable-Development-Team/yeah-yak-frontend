36b91190d0630bf7630cef14ef4538a5824f654e
hibamtol1 <hibamtol1@gmail.com>
2023-12-26 23:34:02 +0900

    관리자 화면 상태 컬럼 각 셀 버튼으로 변경 버튼 클릭시 모달 오픈

--- src/components/templates/Admin/index.jsx
+++ src/components/templates/Admin/index.jsx
@@ -1,13 +1,18 @@
-// TableWithApi.js
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Table from '@modules/Table';
 import Button from '@atoms/Button';
+import Modal from '@modules/Modal';
 import { SERVER_HOST } from '@config/config';
+import { useModalContext } from '@modules/Modal/ModalContext';
+import Input from '@atoms/Input';
+import { FlexContainer } from '@atoms/Flex';
+import SelectList from '@atoms/SelectList';
 
 export default function Confirm() {
+  const { openModal, closeModal } = useModalContext();
   const [reservationData, setReservationData] = useState([]);
+  const [selectedReservation, setSelectedReservation] = useState(null);
 
   const fetchData = async () => {
     try {
@@ -26,21 +31,46 @@ export default function Confirm() {
     if (!data || !data.item) {
       return [];
     }
-  
+
     return data.item.map((item) => ({
       시작일자: item.startDate,
       종료일자: item.endDate,
       신청일: item.createdDate,
       예약자명: item.name,
       전화번호: item.phoneNumber,
-      상태: <Button>{item.status.description}</Button>,
+      상태: (
+        <Button onClick={() => handleReservationClick(item)}>
+          {item.status.description}
+        </Button>
+      ),
     }));
   };
-   
+
+  const handleReservationClick = (reservation) => {
+    setSelectedReservation(reservation);
+    openModal('statusModal');
+  };
+
+  const closeModalHandler = () => {
+    setSelectedReservation(null);
+    closeModal('statusModal');
+  };
 
   return (
-    <div>
+    <FlexContainer $justifyContent="space-between" $alignItems="center">
       <Table data={mapDataToColumns(reservationData)} />
-    </div>
+      {selectedReservation && (
+        <Modal title="상태 확인" modalId="statusModal" hasButton={false}>
+          <FlexContainer $column $gap="16px">
+            <Input label="예약자명" readOnly value={selectedReservation.name} />
+            <Input label="전화번호" readOnly value={selectedReservation.phoneNumber} />
+            <SelectList label="상태" value={selectedReservation.status.description} />
+          </FlexContainer>
+          <FlexContainer $justifyContent="flex-end">
+            <Button onClick={closeModalHandler}>확인</Button>
+          </FlexContainer>
+        </Modal>
+      )}
+    </FlexContainer>
   );
 }
