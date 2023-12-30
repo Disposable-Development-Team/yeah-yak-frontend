487d8ea16102ea4a603f6e87e6e5404f60cd7c21
makerseok <maker7788@gmail.com>
2023-12-31 02:40:46 +0900

    Table ui 변경

--- src/components/modules/Table/index.jsx
+++ src/components/modules/Table/index.jsx
@@ -5,25 +5,32 @@ import styled from 'styled-components';
 
 const TableWrapper = styled.table`
   width: 100%;
-  margin: 16px 0;
   border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+  table-layout: fixed;
 `;
 
 const TableHeader = styled.th`
-  background-color: #f2f2f2;
-  padding: 12px;
+  padding: 12px 15px;
+  background-color: #014034;
+  color: #ffffff;
   text-align: left;
-  border: 1px solid #ddd;
 `;
 
 const TableCell = styled.td`
-  padding: 12px;
-  border: 1px solid #ddd;
+  padding: 12px 15px;
 `;
 
 const TableRow = styled.tr`
+  border-bottom: 1px solid #dddddd;
   &:nth-child(even) {
-    background-color: #f9f9f9;
+    background-color: #e6e6e6;
+  }
+  &:last-of-type {
+    border-bottom: 2px solid #014034;
   }
 `;
 
@@ -46,7 +53,7 @@ const Table = ({ data }) => {
         {data.map((row, rowIndex) => (
           <TableRow key={rowIndex}>
             {headers.map((header, index) => (
-              <TableCell key={index}>{typeof row[header] !== 'object' ? row[header] : ''}</TableCell>
+              <TableCell key={index}>{row[header]}</TableCell>
             ))}
           </TableRow>
         ))}
